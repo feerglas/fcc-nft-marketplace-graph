5e543bbace9fbf80435c804122649a696cd149b0
Yves Tscherry <info@feerglas.com>
2022-06-13 14:52:47 +0200

    refactor

--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -6,70 +6,81 @@ import {
 } from "../generated/NFTMarketplace/NFTMarketplace"
 import { ItemListed, ActiveItem, ItemBought, ItemCanceled } from "../generated/schema"
 
-function getIdFromEventParams(tokenId: BigInt, nftAddress: Address): string {
-  return tokenId.toHexString() + nftAddress.toHexString();
-}
-
-export function handleItemBought(event: ItemBoughtEvent): void {
-  let itemBought = ItemBought.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
-  let activeItem = ActiveItem.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
-
-  if (!itemBought) {
-    itemBought = new ItemBought(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
+export function handleItemListed(event: ItemListedEvent): void {
+  let itemListed = ItemListed.load(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  )
+  let activeItem = ActiveItem.load(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  )
+  if (!itemListed) {
+      itemListed = new ItemListed(
+          getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+      )
   }
+  if (!activeItem) {
+      activeItem = new ActiveItem(
+          getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+      )
+  }
+  itemListed.seller = event.params.seller
+  activeItem.seller = event.params.seller
 
-  itemBought.buyer = event.params.buyer;
-  itemBought.nftAddress = event.params.nftAddress;
-  itemBought.tokenId = event.params.tokenId;
-  activeItem!.buyer = event.params.buyer;
+  itemListed.nftAddress = event.params.nftAddress
+  activeItem.nftAddress = event.params.nftAddress
 
-  itemBought.save();
-  activeItem!.save();
+  itemListed.tokenId = event.params.tokenId
+  activeItem.tokenId = event.params.tokenId
+
+  itemListed.price = event.params.price
+  activeItem.price = event.params.price
+
+  itemListed.save()
+  activeItem.save()
 }
 
 export function handleItemCanceled(event: ItemCanceledEvent): void {
-  let itemCanceled = ItemCanceled.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
-  let activeItem = ActiveItem.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
-
+  let itemCanceled = ItemCanceled.load(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  )
+  let activeItem = ActiveItem.load(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  )
   if (!itemCanceled) {
-    itemCanceled = new ItemCanceled(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
+      itemCanceled = new ItemCanceled(
+          getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+      )
   }
+  itemCanceled.seller = event.params.seller
+  itemCanceled.nftAddress = event.params.nftAddress
+  itemCanceled.tokenId = event.params.tokenId
+  activeItem!.buyer = Address.fromString("0x000000000000000000000000000000000000dEaD")
 
-  itemCanceled.seller = event.params.seller;
-  itemCanceled.nftAddress = event.params.nftAddress;
-  itemCanceled.tokenId = event.params.tokenId;
-
-  activeItem!.buyer = Address.fromString('0x000000000000000000000000000000000000dEaD');
-
-  itemCanceled.save();
-  activeItem!.save();
+  itemCanceled.save()
+  activeItem!.save()
 }
 
-export function handleItemListed(event: ItemListedEvent): void {
-  let itemListed = ItemListed.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
-  let activeItem = ActiveItem.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
-
-  if (!itemListed) {
-    itemListed = new ItemListed(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
+export function handleItemBought(event: ItemBoughtEvent): void {
+  let itemBought = ItemBought.load(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  )
+  let activeItem = ActiveItem.load(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  )
+  if (!itemBought) {
+      itemBought = new ItemBought(
+          getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+      )
   }
+  itemBought.buyer = event.params.buyer
+  itemBought.nftAddress = event.params.nftAddress
+  itemBought.tokenId = event.params.tokenId
+  activeItem!.buyer = event.params.buyer
 
-  if (!activeItem) {
-    activeItem = new ActiveItem(getIdFromEventParams(event.params.tokenId, event.params.nftAddress));
-  }
+  itemBought.save()
+  activeItem!.save()
+}
 
-  itemListed.seller = event.params.seller;
-  activeItem.seller = event.params.seller;
-
-  itemListed.nftAddress = event.params.nftAddress;
-  activeItem.nftAddress = event.params.nftAddress;
-
-
-  itemListed.tokenId = event.params.tokenId;
-  activeItem.tokenId = event.params.tokenId;
-
-  itemListed.price = event.params.price;
-  activeItem.price = event.params.price;
-
-  itemListed.save();
-  activeItem.save();
+function getIdFromEventParams(tokenId: BigInt, nftAddress: Address): string {
+  return tokenId.toHexString() + nftAddress.toHexString()
 }
